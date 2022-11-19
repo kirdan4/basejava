8eb6bbf826fd56cb2a0323588daf11525d02bf47
Savtsov <savtsovao@yandex.ru>
2022-11-19 12:18:17 +0300

    ДЗ для Lesson_1 (BaseJava) v.1.0.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,50 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private static int numberResumes;
 
     void clear() {
+        Arrays.fill(storage, 0, numberResumes, null);
+        numberResumes = 0;
+        System.out.println("База резюме очищена.");
     }
 
     void save(Resume r) {
+        storage[numberResumes] = r;
+        numberResumes++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < numberResumes; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < numberResumes; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                numberResumes--;
+                System.arraycopy(storage, (i + 1), storage, i, numberResumes - i);
+                storage[numberResumes] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, numberResumes);
     }
 
     int size() {
-        return 0;
+        return numberResumes;
     }
 }
